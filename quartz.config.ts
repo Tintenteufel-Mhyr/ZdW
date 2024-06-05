eacbe58588005df8ff649142712162751b1c6402
Tintenteufel-Mhyr <tintenteufel.maps@gmail.com>
2024-06-05 22:37:22 +0200

    Update quartz.config.ts
    changed Font

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Zeit der Wölfe - Buch IV",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
@@ -22,8 +22,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Special Elite",
+        body: "Special Elite",
         code: "IBM Plex Mono",
       },
       colors: {
